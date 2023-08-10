bb50a47fdf33485a9f6da43fef8388f65d42aa49
Liam <liam.alton@signaturegroup.com.au>
2023-08-11 08:19:01 +1000

    Aligned env variables with Amply config

--- app/api/auth/context.js
+++ app/api/auth/context.js
@@ -2,8 +2,8 @@
 import { CognitoUserPool} from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: process.env.AWS_COGNITO_POOL_ID,
-  ClientId: process.env.AWS_COGNITO_CLIENT_ID
+  UserPoolId: process.env.COGNITO_POOL_ID,
+  ClientId: process.env.COGNITO_CLIENT_ID
 }
 
 const Context = {userPool: new CognitoUserPool(poolData)}
