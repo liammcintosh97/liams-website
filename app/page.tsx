00d576a8ccae38d3fbb80dcaf0be14e2b66ece77
Liam <liam.alton@signaturegroup.com.au>
2024-04-27 21:18:07 +1000

    added projects route

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 /** @module RootPage */
 import "./globals.scss"
 import './page.module.scss'
-import ProjectCard from "./components/ProjectCard"
 import { openGraphImage, openGraphBasicFields} from './shared-metadata'
 import React from "react"
-import { getProjects } from "./util/projects"
 
 const title = 'Home'
 const description = "The home page of Liam's website"
@@ -25,19 +23,9 @@ export const metadata = {
  * @returns {Promise<JSX.Element>}
  */
 export default async function Page(): Promise<JSX.Element>{
-  const projects = await getProjects()
   return (
     <div>
       <h1>Home Page</h1>
-      {projects.map((e)=>{
-        return(
-          <ProjectCard
-            key={"project_card_" + e.slug}
-            title={e.title}
-            slug={e.slug}
-          />
-        )
-      })}
     </div>
   )
 }
